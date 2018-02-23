9ad8953447cbb42d0a493ad48af1cbdb53c05684
Joel McFadden <mcf.joel@gmail.com>
2018-02-23 14:30:26 -0700

    initial commit

---
+++ assign3/chat-example/index.js
@@ -0,0 +1,13 @@
+// Sample chat application code taken from
+// https://socket.io/get-started/chat/
+
+var app = require('express')();
+var http = require('http').Server(app);
+
+app.get('/', function(req, res) {
+    res.send('<h1>Hello world</h1>');
+});
+
+http.listen(3000, function() {
+    console.log('listening on *:3000');
+});
